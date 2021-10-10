665cd08f32c765498ccc8305087b0d3504da295f
Leo Li <liaodahaoren888@gmail.com>
2021-10-10 01:54:44 -0700

    Chapter 3.1

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,52 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Ao Li on 2021-10-10.
+//
+
+import Foundation
+import SwiftUI
+
+struct DetailView: View{
+    let scrum: DailyScrum
+    var body: some View{
+        List{
+            Section(header: Text("Meeting Info")){
+                NavigationLink(destination: MeetingView()){
+                    Label("Start Meeting", systemImage: "timer")
+                        .accessibilityLabel(Text("Start meeting"))
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
+                
+                HStack{
+                    Label("Length", systemImage: "clock")
+                        .accessibilityLabel(Text("Meeting length"))
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                HStack{
+                    Label("Color", systemImage: "paintpalette")
+                    Spacer()
+                    Image(systemName: "checkmark.circle.fill")
+                }
+            }
+            Section(header: Text("Attendees")){
+                ForEach(scrum.attendees, id: \.self){ attendee in
+                    Label(attendee, systemImage: "person")
+                }
+            }
+        }
+        .listStyle(InsetGroupedListStyle())
+        .navigationTitle(scrum.title)
+    }
+}
+
+struct DetailView_Preview: PreviewProvider{
+    static var previews: some View{
+        NavigationView{
+            DetailView(scrum: DailyScrum.data[0])
+        }
+    }
+}
--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -28,9 +28,9 @@ struct DailyScrum: Identifiable{
 extension DailyScrum {
     static var data: [DailyScrum] {
         [
-            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
-            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
-            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color.yellow),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color.orange),
+            DailyScrum(title: "Web Dev", attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color.red)
         ]
     }
 }
